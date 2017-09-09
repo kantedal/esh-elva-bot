d51020528cb226f39bf217e51febfddd76079d8d
Hedlundaren <simhe813@student.liu.se>
2017-09-09 13:04:09 +0200

    init messenger webhook

--- src/facebook-webhook.ts
+++ src/facebook-webhook.ts
@@ -1,5 +1,29 @@
 import * as express from 'express'
 
+const bodyParser = require('body-parser')
+const request = require('request')
+
 export const initFacebookMessengerWebhook = (app: express.Application) => {
 
+    app.set('port', (process.env.PORT || 5000))
+    app.use(bodyParser.urlencoded({extended: false}))
+    app.use(bodyParser.json())
+
+    // Routes
+    app.get('/', (req, res) => {
+        res.send('Hi, I am Elva.')
+
+    })
+
+    // Facebook
+    app.get('/webhook/', (req, res) =>{
+        if(req.query['hub.verify_token'] === process.env.MESSENGER_VERIFY_TOKEN){
+            res.send(req.query['hub.challenge'])
+        }
+        res.send('Wrong token.')
+    })
+
+    app.listen(app.get('port'), () => {
+        console.log('running: port')
+    })
 }
