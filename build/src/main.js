953284632e42981c9bd2399f02cb7279fc1db618
Hedlundaren <simhe813@student.liu.se>
2017-09-09 15:03:05 +0200

    messenger response try

--- build/src/main.js
+++ build/src/main.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const tslib_1 = require("tslib");
+const facebook_webhook_1 = require("./facebook-webhook");
 require('dotenv').config();
 const chat_client_1 = require("./chat-client");
 const start_express_app_1 = require("./start-express-app");
@@ -11,6 +12,7 @@ const startServer = () => tslib_1.__awaiter(this, void 0, void 0, function* () {
     const App = yield start_express_app_1.startExpressApp();
     apiai_webhook_1.initApiAiWebhook(App);
     chat_client_1.startChatClient();
+    facebook_webhook_1.initFacebookMessengerWebhook(App);
 });
 startServer();
 //# sourceMappingURL=main.js.map
--- src/facebook-webhook.ts
+++ src/facebook-webhook.ts
@@ -1,13 +1,46 @@
 import * as express from 'express'
+import {error} from 'util'
 
-const bodyParser = require('body-parser')
 const request = require('request')
+const token: string = process.env.FACEBOOK_PAGE_ACCESS_TOKEN
 
 export const initFacebookMessengerWebhook = (app: express.Application) => {
 
+    app.post('/webhook/', (req, res) => {
+        const messaging_events = req.body.entry[0].messaging
+        for(const event of messaging_events){
+            const sender = event.sender.id
+            if(event.message && event.message.text) {
+                const text = event.message.text
+                sendText(sender, 'Text echo: ' + text.substring(0,100))
+            }
+        }
 
+        res.sendStatus(200)
+    })
 
     app.listen(app.get('port'), () => {
         console.log('running: port')
+    })
+}
+
+const sendText = (sender, text) => {
+    const messageData = {text}
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: token},
+        method: 'POST',
+        json: {
+            recipient: {id: sender},
+            message: messageData
+        }
+
+        /*(error, response, body) => {
+        if(error){
+            console.log('sending error')
+        }else if(response.body.error){
+            console.log('response body error')
+        }*/
+
     })
 }
