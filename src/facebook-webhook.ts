1aba922e8248e2237a2237a27525b2f90ec7b7a2
Hedlundaren <simhe813@student.liu.se>
2017-09-09 13:38:19 +0200

    webhook get added

--- src/facebook-webhook.ts
+++ src/facebook-webhook.ts
@@ -5,13 +5,7 @@ const request = require('request')
 
 export const initFacebookMessengerWebhook = (app: express.Application) => {
 
-    // Facebook
-    app.get('/webhook/', (req, res) => {
-        if(req.query['hub.verify_token'] === process.env.MESSENGER_VERIFY_TOKEN) {
-            res.send(req.query['hub.challenge'])
-        }
-        res.send('Wrong token.')
-    })
+
 
     app.listen(app.get('port'), () => {
         console.log('running: port')
