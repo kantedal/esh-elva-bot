37ffb7ef4f42c9af3d60557395d9b74557ef0088
Hedlundaren <simhe813@student.liu.se>
2017-09-09 16:40:37 +0200

    log req

--- build/src/facebook-webhook.js
+++ build/src/facebook-webhook.js
@@ -1,5 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const tslib_1 = require("tslib");
+const api_ai_1 = require("./chat-logics/api-ai");
 const request = require('request');
 const token = 'EAAVvTioZBgqYBAKjMA5ZBPjTl3zIY1klJzd3aeePmEoaSvIGdp8dDouen0b9mJNQ4lDkdb9eVhKpN1I1omSuzIFeBup3e77KM2IqdWxrfgDobbXlT8VuGJw79dx8aZAKAi3agv6QNUu9MtTnfxk105Don0ycrwaDOfq0ahyegZDZD';
 exports.initFacebookMessengerWebhook = (app) => {
@@ -7,24 +9,26 @@ exports.initFacebookMessengerWebhook = (app) => {
         res.send('Hi, I am Elva...  ');
         console.log('Hi I am Elva');
     });
-    app.get('/webhook/', (req, res) => {
+    app.get('/messenger-webhook/', (req, res) => {
         if (req.query['hub.verify_token'] === 'detejubaraetthack') {
             res.send(req.query['hub.challenge']);
         }
         res.send('Wrong token.');
     });
-    app.post('/webhook/', (req, res) => {
-        console.log('Inne!');
+    app.post('/messenger-webhook/', (req, res) => tslib_1.__awaiter(this, void 0, void 0, function* () {
+        console.log(req);
+        console.log(res);
         const messaging_events = req.body.entry[0].messaging;
         for (const event of messaging_events) {
             const sender = event.sender.id;
             if (event.message && event.message.text) {
                 const text = event.message.text;
-                sendText(sender, 'Hej');
+                const responseMessage = yield api_ai_1.sendMessage(text, 'session-token', { userId: 'heja blavitt!!' });
+                sendText(sender, responseMessage);
             }
         }
         res.sendStatus(200);
-    });
+    }));
     app.listen(app.get('port'), () => {
         console.log('running: port');
     });
--- src/facebook-webhook.ts
+++ src/facebook-webhook.ts
@@ -1,5 +1,6 @@
 import * as express from 'express'
 import {error} from 'util'
+import {sendMessage} from "./chat-logics/api-ai";
 
 const request = require('request')
 // const token: string = process.env.FACEBOOK_PAGE_ACCESS_TOKEN
@@ -14,21 +15,24 @@ export const initFacebookMessengerWebhook = (app: express.Application) => {
     })
 
     // Facebook
-    app.get('/webhook/', (req, res) => {
+    app.get('/messenger-webhook/', (req, res) => {
         if(req.query['hub.verify_token'] === 'detejubaraetthack') {
             res.send(req.query['hub.challenge'])
         }
         res.send('Wrong token.')
     })
 
-    app.post('/webhook/', (req, res) => {
-        console.log('Inne!')
+    app.post('/messenger-webhook/', async (req, res) => {
+        console.log(req)
+        console.log(res)
         const messaging_events = req.body.entry[0].messaging
+
         for(const event of messaging_events){
             const sender = event.sender.id
             if(event.message && event.message.text) {
                 const text = event.message.text
-                sendText(sender, 'Hej')
+                const responseMessage: string = await sendMessage(text, 'session-token', {userId: 'heja blavitt!!'})
+                sendText(sender, responseMessage)
             }
         }
 
