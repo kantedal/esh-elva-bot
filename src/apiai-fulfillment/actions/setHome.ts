97ae070951ea18afeba51e6d49f5d7f5ac200c3f
kantedal <kantedal@gmail.com>
2017-09-10 10:08:41 +0200

    Try catch added to setHome

--- src/apiai-fulfillment/actions/setHome.ts
+++ src/apiai-fulfillment/actions/setHome.ts
@@ -2,6 +2,11 @@
 import {getDatabaseUser, getUserFromSessionId, setSessionId, setUserProperty} from '../../chat-logics/databaseUser'
 
 export const setHome = async (sessionId: string, homeAddress: string) => {
-  await setUserProperty(sessionId, 'homeAddress', homeAddress)
-  return `Okey, saved your address! You live at ${homeAddress} 🏡`
+  try {
+    await setUserProperty(sessionId, 'homeAddress', homeAddress)
+    return `Okey, saved your address! You live at ${homeAddress} 🏡`
+  } catch (err) {
+    console.log(err)
+    return 'Something went wrong with setting your address 😞'
+  }
 }
