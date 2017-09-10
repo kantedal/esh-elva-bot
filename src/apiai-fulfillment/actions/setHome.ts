303e972e79044373f7dfe7c83824f9ebe71aecf4
kantedal <kantedal@gmail.com>
2017-09-10 09:33:13 +0200

    Updated setHome

--- src/apiai-fulfillment/actions/setHome.ts
+++ src/apiai-fulfillment/actions/setHome.ts
@@ -1,5 +1,6 @@
 import {getDatabaseUser, getUserFromSessionId, setSessionId, setUserProperty} from '../../chat-logics/databaseUser'
 
 export const setHome = async (sessionId: string, homeAddress: string) => {
-  return setUserProperty(sessionId, 'homeAddress', homeAddress)
+  await setUserProperty(sessionId, 'homeAddress', homeAddress)
+  return {}
 }
